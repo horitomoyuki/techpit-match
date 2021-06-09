c1b52f340380fcd396418a5e5ffcd2e33891c4f8
horitomoyuki <eziltom0131@gmail.com>
2021-06-09 17:55:47 +0900

    修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,6 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to '/users/index'
+    redirect_to '/users/show'
   end
 end
